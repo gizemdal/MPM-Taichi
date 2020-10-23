25c6be6d83fd1a520017c0890405a4d801fabf20
Xuan Li <xuan.shayne.li@gmail.com>
2020-10-22 23:45:04 -0400

    hw2

--- Projects/mass_spring/main.cpp
+++ Projects/mass_spring/main.cpp
@@ -8,6 +8,7 @@
 #include <random>
 #include <chrono>
 #include <unordered_set>
+#include <iomanip>
 
 #include "SimulationDriver.h"
 
@@ -22,7 +23,7 @@ int main(int argc, char* argv[])
     // set up mass spring system
     T youngs_modulus = 0;
     T damping_coeff = 0; 
-    T dt = 0;
+    T dt = T(1/24.);
 
     // node data
     std::vector<T> m;
@@ -40,36 +41,74 @@ int main(int argc, char* argv[])
         exit(0);
     }
 
-    if (strcmp(argv[1], "0") == 0) // cloth case
-    {
-        // TODO
+    if (strcmp(argv[1], "1") == 0) { // bunny case
+        youngs_modulus = 0.1; // TODO: iterate in [0.1, 1, 10, 100, 1000]
+        damping_coeff = 2;
+        // TODO: 
         /* 
-            1. Create node data: position, mass, velocity
-            2. Fill segments and rest_length, including struct springs, shearing springs and bending springs.
-            3. Choose proper youngs_modulus, damping_coeff and dt.
-            4. Set boundary condition (node_is_fixed) and helper function (to achieve moving boundary condition).
-            5. Generate quad mesh for rendering.
+            1. Copy the loading codes from your hw1. Fix two ears (2140, 2346) only, and you don't need helper function here.
+            2. Set the initial velocities of non_fixed_nodes to be (10, 0, 0)
+            
+            The output folder will automatically renamed by bunny_[youngs_modulus], don't worry about overwriting.
         */
-        driver.helper = [&](T t, T dt) {
-            // TODO
-        };
-        driver.test="cloth";
+        std::stringstream ss;
+        ss << std::fixed << std::setprecision(2) << youngs_modulus;
+        driver.test="bunny_"+ss.str();
     }
 
-    else if (strcmp(argv[1], "1") == 0) // volumetric bunny case
-    { 
-        // TODO
-        /* 
-            1. Create node data from data/points: The first line indicates the number of points and dimension (which is 3). 
-            2. Fill segments and rest_length from data/cells: The first line indicates the number of tetrahedra and the number of vertices of each tet (which is 6). Each edge in this tetrahedral mesh will be a segment. Be careful not to create duplicate edges. 
-            3. Choose proper youngs_modulus, damping_coeff, dt; 
-            4. Set boundary condition (node_is_fixed) and helper function (to achieve moving boundary condition).
-        */
+    else if (strcmp(argv[1], "2") == 0) { //brush case
+        driver.gravity.setZero();
+        driver.collision_stiffness = 0.1;
+        youngs_modulus = 10000;
+        damping_coeff = 100; // 0
+        int N = 32; // z direction
+        int M = 4; // y direction
+        int L = 32; // x direction
+        int N_points = N*M*L;
+        T dx = (T)0.1/(N-1);
+        m.resize(N_points);
+        x.resize(N_points);
+        v.resize(N_points);
+        node_is_fixed = std::vector<bool>(N_points, false);
+        for(int i=0; i<N; i++){ // z
+            for(int j=0; j<M; j++) { // y
+                for (int k=0; k<L; k++) { // x
+                    int id = i * M * L + j * L + k;
+                    m[id] = (T)0.001/N_points;
+                    x[id](2) = i*dx;
+                    x[id](1) = j*dx;
+                    x[id](0) = k*dx;
+                    v[id] = TV::Zero();
+                    if (k <= 2) node_is_fixed[id] = true;
+                    // struct spring
+                    if (k > 0) { 
+                        segments.push_back(Eigen::Matrix<int,2,1>(id, id-1));
+                        rest_length.push_back((x[id]-x[id-1]).norm());
+                    }
+                    // bending spring
+                    if (k > 1) {
+                        segments.push_back(Eigen::Matrix<int,2,1>(id, id-2));
+                        rest_length.push_back((x[id]-x[id-2]).norm());
+                    }
+                }
+            }
+        }
+
+        driver.sphere_radius = 0.04;
+        driver.sphere_center = TV(0.07, -0.045, 0.05);
 
         driver.helper = [&](T t, T dt) {
-            // TODO
+            if(t < 4) {
+                // driver.sphere_center = TV(0.12, t/4. * 0.15 + (1-t/4.) * (-0.06), 0.05);
+                for(size_t i = 0; i < driver.ms.x.size(); ++i) {
+                    if (driver.ms.node_is_fixed[i]) {
+                        driver.ms.target_x[i](1) -= 0.15 / 4 * dt;
+                    }
+                }
+            }
         };
-        driver.test="bunny";
+
+        driver.test="brush";
     }
 
     else {
@@ -84,12 +123,13 @@ int main(int argc, char* argv[])
     driver.ms.m = m;
     driver.ms.v = v;
     driver.ms.x = x;
+    driver.ms.target_x = x;
     driver.ms.youngs_modulus = youngs_modulus;
     driver.ms.damping_coeff = damping_coeff;
     driver.ms.node_is_fixed = node_is_fixed;
     driver.ms.rest_length = rest_length;
 
-    driver.run(120);
+    driver.run(180);
 
     return 0;
 }
